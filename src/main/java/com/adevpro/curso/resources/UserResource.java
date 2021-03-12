e84fb8707cbe3ae68340c71b3e9a888701f6a597
AlvesDlper <arthur_s.a@hotmail.com.br>
2021-03-12 01:30:15 -0300

    Service layer, component registration

--- src/main/java/com/adevpro/curso/resources/UserResource.java
+++ src/main/java/com/adevpro/curso/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.adevpro.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adevpro.curso.entities.User;
+import com.adevpro.curso.services.UserService;
 
 @RestController // Anotation responsável por indicar que a classe é implementada por um
 @RequestMapping(value = "/users") // Anotation que mapeia (dá nome para) o caminho para acesso à este Classe (end point).
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
+	public ResponseEntity<List<User>> findAll() {
 
-		User u = new User(1L, "Arthur", "Arthur@hotmail.com", "991528225", "123456");
-		return ResponseEntity.ok().body(u);
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
 	}
 }
